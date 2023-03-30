bd8785902480735e2d52cb0f5b00d9ff0193eadb
Manuel Ulate <fr3zs09@gmail.com>
2023-03-30 10:58:40 -0600

    dynamic validations

--- src/03-forms/pages/DynamicFormPage.tsx
+++ src/03-forms/pages/DynamicFormPage.tsx
@@ -11,12 +11,21 @@ for ( const input of formJson ) {
 
     if( !input.validations ) continue;
 
-    let schema = Yup.object();
+    let schema = Yup.string();
 
     for ( const rule of  input.validations ) {
         if( rule.type === 'required' ) {
             schema = schema.required('Este campo es requerido');
         }
+
+        if( rule.type === 'minLength' ) {
+            schema = schema.min( ( rule as any ).value || 2, `Minimo de ${ ( rule as any ).value || 2 } caracteres`);
+        }
+
+        if( rule.type === 'email' ) {
+            schema = schema.email( 'Revise el formato del email' );
+        }
+
     }
 
     requiredFiles[ input.name ] = schema;
